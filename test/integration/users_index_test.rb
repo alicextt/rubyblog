f11046576bd1379884c7ee12a31f7fba4f7c3cd3
txu <txu@godaddy.com>
2018-03-17 12:01:14 -0700

    init

---
+++ test/integration/users_index_test.rb
@@ -0,0 +1,13 @@
+require 'test_helper'
+
+class UsersIndexTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+
+  def setup
+    @user = users(:michael)
+  end
+
+
+end
