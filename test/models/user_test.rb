58653f71d62ab3114165b41a526605b0452cf53c
txu <txu@godaddy.com>
2018-03-17 20:52:38 -0700

    chap 11

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -25,6 +25,6 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "authenticated? should return false for a user with nil digest" do
-    assert_not @user.authenticated?('')
+    assert_not @user.authenticated?(:remember, '')
   end
 end
